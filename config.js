c4e5692125aca0bbaaaa6d7d7a04bfdda8983bc6
Shubham Saxena <12321254+saxenashubham@users.noreply.github.com>
2026-08-11 22:01:06 -0500

    Update and rename config.example.js to config.js

--- config.js
+++ config.js
@@ -4,19 +4,19 @@
 window.PACKCHECK = {
   // Firebase console -> Project settings -> your web app -> "SDK setup and configuration"
   firebase: {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAve08JHxeRAAAv6CXwA26jno0jbyf1U_o",
+    authDomain: "packcheck-b542a.firebaseapp.com",
+    projectId: "packcheck-b542a",
+    storageBucket: "packcheck-b542a.firebasestorage.app",
+    messagingSenderId: "398744155641",
+    appId: "1:398744155641:web:e6651ad578f4746aaeb928"
   },
 
   // The two (or more) Google accounts allowed to use this app.
   // These MUST also be listed in your Firestore rules (see firestore.rules).
   allowedEmails: [
-    "you@gmail.com",
-    "partner@gmail.com"
+    "shubhamsaxena1492@gmail.com",
+    "shubhangi9237@gmail.com"
   ],
 
   // Collection prefix. Lets you reuse an existing Firebase project without
